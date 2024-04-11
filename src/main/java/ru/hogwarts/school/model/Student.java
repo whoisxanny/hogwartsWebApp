b0becfd0ec15444e85bca33e3fd669f639cf49f8
whoisxanny <tuffx4nny@gmail.com>
2024-04-11 11:48:51 +0500

    sql3?

--- src/main/java/ru/hogwarts/school/model/Student.java
+++ src/main/java/ru/hogwarts/school/model/Student.java
@@ -39,6 +39,9 @@ public class Student {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
--- src/main/resources/sql/scripts423.sql
+++ src/main/resources/sql/scripts423.sql
@@ -1,7 +1,7 @@
 SELECT student.name, student.age
 FROM STUDENT
-JOIN: INNER JOIN FACULTY ON student.faculty_id = faculty.faculty_id;
+INNER JOIN FACULTY ON student.faculty_id = faculty.faculty_id;
 
 SELECT student.name
 FROM STUDENT
-JOIN: INNER JOIN AVATAR ON student.avatar_avatar_id = avatar_id;
+INNER JOIN AVATAR ON student.avatar_avatar_id = avatar_id;

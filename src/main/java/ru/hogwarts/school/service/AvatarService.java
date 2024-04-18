37c530faf2b23e6e771930de70f43f220873ca37
whoisxanny <tuffx4nny@gmail.com>
2024-04-18 18:41:46 +0500

    sql 6

--- src/main/java/ru/hogwarts/school/service/AvatarService.java
+++ src/main/java/ru/hogwarts/school/service/AvatarService.java
@@ -2,6 +2,8 @@ package ru.hogwarts.school.service;
 
 
 import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -19,6 +21,7 @@ import java.nio.file.Path;
 import java.util.Collection;
 import java.util.Objects;
 
+
 import static java.nio.file.StandardOpenOption.CREATE_NEW;
 
 @Service
@@ -27,6 +30,8 @@ public class AvatarService {
 
     @Value("avatars")
     private String avatarsDir;
+
+     Logger logger = LoggerFactory.getLogger(AvatarService.class);
 
     private final StudentService studentService;
     private final AvatarRepository avatarRepository;
@@ -37,6 +42,7 @@ public class AvatarService {
     }
 
     public void uploadAvatar(Long studentId, MultipartFile file) throws IOException {
+        logger.info("Was invoked method to upload avatar");
         Student student = studentService.getStudent(studentId);
 
         Path filePath = Path.of(avatarsDir, studentId + "." + getExtension(Objects.requireNonNull(file.getOriginalFilename())));
@@ -63,6 +69,7 @@ public class AvatarService {
 
 
     public byte[] generateDataForBase(Path filePath) throws IOException {
+        logger.info("Was invoked method for to create avatarpath in db");
         try (
                 InputStream is = Files.newInputStream(filePath);
                 BufferedInputStream bis = new BufferedInputStream(is, 1024);
@@ -80,17 +87,20 @@ public class AvatarService {
     }
 
     public Avatar findAvatar(Long studentId) {
+        logger.info("Was invoked method to find avatar by student_id");
         return avatarRepository.findByStudentId(studentId).orElse(new Avatar());
     }
 
 
     public Collection<Avatar> getAllAvatars(Integer pageNumber, Integer pageSize) {
+        logger.info("Was invoked method to find all avatars");
         PageRequest page = PageRequest.of(pageNumber-1,pageSize);
         return avatarRepository.findAll(page).getContent();
     }
 
 
     public String getExtension(String fileName) {
+        logger.info("Was invoked method to create fileName");
         return fileName.substring(fileName.lastIndexOf(".") + 1);
     }
 
--- src/main/java/ru/hogwarts/school/service/StudentService.java
+++ src/main/java/ru/hogwarts/school/service/StudentService.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
@@ -11,6 +13,7 @@ import java.util.*;
 @Service
 public class StudentService {
 
+    Logger logger = LoggerFactory.getLogger(StudentService.class);
     private final StudentRepository studentRepository;
 
     public StudentService(StudentRepository studentRepository) {
@@ -18,22 +21,27 @@ public class StudentService {
     }
 
     public Student createStudent(Student student) {
+        logger.debug("Was invoked method for create student");
         return studentRepository.save(student);
     }
 
     public Student getStudent(Long idG) {
+        logger.debug("Was invoked method for find student by id {}", idG);
         return studentRepository.findById(idG).orElseThrow(RuntimeException::new);
     }
 
     public Student updateStudent(Student student) {
+        logger.debug("Was invoked method for update student by student-body {}", student);
         return studentRepository.save(student);
     }
     
     public void deleteStudent(Long idD) {
+        logger.debug("Was invoked method for delete student by id {}", idD);
         studentRepository.deleteById(idD);
     }
 
     public Collection<Student> filteredByAge(Integer age) {
+        logger.debug("Was invoked method for filter students by age {}", age);
         List<Student> acceptables = new ArrayList<>();
         for (Student student : studentRepository.findAll()) {
             if (student.getAge() >= age) {
@@ -44,29 +52,36 @@ public class StudentService {
     }
 
     public Integer getAmountOfStudents() {
+        logger.debug("Was invoked method to get amount of students");
         return studentRepository.getAmountOfStudents();
     }
 
     public Double getAverageAgeFromStudents() {
+        logger.debug("Was invoked method to get average age of students");
         return studentRepository.getAverageAgeOfStudents();
     }
 
     public Collection<Student> findAll() {
+        logger.debug("Was invoked method to get list of all students");
         return studentRepository.findAll();
     }
     public Collection<Student> findStudentBetweenAges(int min, int max) {
+        logger.debug("Was invoked method to get students in ages between {} and {}", min, max);
         return studentRepository.findStudentBetweenAge(min, max);
     }
 
     public Collection<Student> getStudentsByName(String name) {
+        logger.debug("Was invoked method to get student by his/her name {}", name);
         return studentRepository.findByName(name);
     }
 
     public Collection<Student> findLastFiveStudents() {
+        logger.debug("Was invoked method to get five last accepted students");
         return studentRepository.getLastFiveStudents();
     }
 
     public Faculty getStudentFaculty(Integer id) {
+        logger.debug("Was invoked method to get faculty, where student is going to study by students id {}", id);
         return studentRepository.findById(id).getFaculty();
     }
 
